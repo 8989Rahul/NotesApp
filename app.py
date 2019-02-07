fdfed58662896e6bbfb25970929c3d6083f80bbe
8989rahul <daghalerahul0@gmail.com>
2019-02-07 07:34:39 +0530

    incude update,delete,etc routes

--- app.py
+++ app.py
@@ -34,12 +34,34 @@ def about():
 # articles
 @app.route('/articles')
 def articles():
-    return render_template('/articles.html' , articles = Articles)
+ # create cursor
+    cur=mysql.connection.cursor()
 
+    # execute query
+    result = cur.execute("select * from articles")
+
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template('articles.html', articles=articles)
+    else:
+        msg = 'Not Found'
+        return render_template('articles.html', msg=msg)
+
+    # close connection 
+    cur.close()
 # single Article
 @app.route('/article/<string:id>/')
 def article(id):
-    return render_template('/article.html' , id = id)
+    # create cursor
+    cur=mysql.connection.cursor()
+
+    # execute query
+    cur.execute("select * from articles where id = %s", [id])
+
+    article = cur.fetchone()
+
+    return render_template('/article.html' , article=article)
 
 # register form class
 class RegisterForm(Form):
@@ -142,11 +164,117 @@ def logout():
 # dashboard route
 @app.route('/dashboard')
 @is_logged_in
-def dashboard():    
-    return render_template('dashboard.html')
+def dashboard(): 
+
+    # create cursor
+    cur=mysql.connection.cursor()
+
+    # execute query
+    result = cur.execute("select * from articles")
+
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template('dashboard.html', articles=articles)
+    else:
+        msg = 'Not Found'
+        return render_template('dashboard.html', msg=msg)
+
+    # close connection 
+    cur.close()    
 
 
+# Article form class
+class ArticleForm(Form):
+    title = StringField('title',[validators.Length(min=1, max=200)])
+    body= TextAreaField('body',[validators.Length(min=30)])
 
+# Add article route
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():   
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        # add cursor
+        cur = mysql.connection.cursor()
+
+        # execute query
+        cur.execute("insert into articles(title, body, author) values(%s, %s, %s)", (title, body, session['username']))
+ 
+        # commit
+        cur.connection.commit()
+
+        # close connection
+        cur.close()
+
+        flash('Article Created','success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_article.html', form=form)
+
+# update article route
+@app.route('/update_article/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def update_article(id): 
+    # add cursor
+    cur = mysql.connection.cursor()
+
+    # execute query
+    cur.execute("select * from articles where id = %s",[id])
+    
+    article = cur.fetchone()
+
+    # get form
+    form = ArticleForm(request.form)
+
+    #populate article form field
+    form.title.data = article['title']
+    form.body.data = article['body']
+        
+    if request.method == 'POST' and form.validate():
+        title = request.form['title']
+        body = request.form['body']
+
+        # add cursor
+        cur = mysql.connection.cursor()
+
+        # execute query
+        cur.execute("UPDATE articles SET title=%s, body=%s where id=%s",(title, body, id) )
+
+        # commit
+        cur.connection.commit()
+
+        # close connection
+        cur.close()
+
+        flash('Article Updated','success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('update_article.html', form=form)
+
+@app.route('/delete_article/<string:id>', methods=['post'])
+@is_logged_in
+def delete_article(id):
+    # add cursor
+        cur = mysql.connection.cursor()
+
+        # execute query
+        cur.execute(" delete from articles where id = %s ", [id])
+
+        # commit
+        cur.connection.commit()
+
+        # close connection
+        cur.close()
+
+        flash('Article Deleted','success')
+
+        return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.secret_key = 'pass123'
